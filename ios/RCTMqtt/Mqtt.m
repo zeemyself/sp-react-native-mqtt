7de9b969e04e41dadd94ea28ac8fa289c72d6cfa
naviocean <navi.ocean@outlook.com>
2019-01-04 18:10:36 +0700

    add MQTTCFSocketTransport

--- ios/RCTMqtt/Mqtt.m
+++ ios/RCTMqtt/Mqtt.m
@@ -81,10 +81,14 @@
         willMsg = [self.options[@"willMsg"] dataUsingEncoding:NSUTF8StringEncoding];
     }
     if (!self.manager) {
-        dispatch_queue_t queue1 = dispatch_queue_create("com.hawking.app.anchor.mqtt", NULL);
-        self.manager = [[MQTTSessionManager alloc] initWithPersistence:NO maxWindowSize:MQTT_MAX_WINDOW_SIZE maxMessages:MQTT_MAX_MESSAGES maxSize:MQTT_MAX_SIZE maxConnectionRetryInterval:60.0 connectInForeground:NO streamSSLLevel:nil queue: queue1];
+        dispatch_queue_t queue = dispatch_queue_create("com.hawking.app.anchor.mqtt", NULL);
+        self.manager = [[MQTTSessionManager alloc] initWithPersistence:NO maxWindowSize:MQTT_MAX_WINDOW_SIZE maxMessages:MQTT_MAX_MESSAGES maxSize:MQTT_MAX_SIZE maxConnectionRetryInterval:60.0 connectInForeground:NO streamSSLLevel:nil queue: queue];
         self.manager.delegate = self;
-        
+        MQTTCFSocketTransport *transport = [[MQTTCFSocketTransport alloc] init];
+        transport.host = [self.options valueForKey:@"host"];
+        transport.port = [self.options[@"port"] intValue];
+        transport.voip = YES;
+        self.manager.session.transport = transport;
         [self.manager connectTo:[self.options valueForKey:@"host"]
                            port:[self.options[@"port"] intValue]
                             tls:[self.options[@"tls"] boolValue]
