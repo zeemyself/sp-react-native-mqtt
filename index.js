02b1a1ff671329599f8a2f740c9112009c6f65cc
Ioannis Kokkinidis <sudoplz@gmail.com>
2019-03-02 01:17:27 +0200

    Now throwing an error when the user specifies a wrong uri

--- index.js
+++ index.js
@@ -67,6 +67,9 @@ module.exports = {
     if(options.uri) {
       var pattern = /^((mqtt[s]?|ws[s]?)?:(\/\/)([0-9a-zA-Z_\.]*):?(\d+))$/;
       var matches = options.uri.match(pattern);
+      if (!matches) {
+        throw new Error(`Uri passed to createClient ${options.uri} doesn't match a known protocol (mqtt:// or ws://).`);
+      }
       var protocol = matches[2];
       var host = matches[4];
       var port =  matches[5];
