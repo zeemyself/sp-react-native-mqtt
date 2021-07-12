88862eacf7071225cea705cd42d934d9b3f8aa1b
Manatsawin Hanmongkolchai <manatsawin+git@lmwn.com>
2021-07-12 15:56:35 +0700

    Update type for constructor

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,28 @@
 export type QoS = 0 | 1 | 2;
 
+export interface ClientOptions {
+	clientId: string;
+	uri: string;
+	host?: string;
+	port?: number;
+	protocol?: 'mqtt' | 'tcp' | 'wss' | 'mqtts' | 'ws';
+	tls?: boolean;
+	keepalive?: number;	// seconds
+	protocolLevel?: number;
+	clean?: boolean;
+	auth?: boolean;
+	user?: string;	// only used when auth is true
+	pass?: string;	// only used when auth is true
+	will?: boolean;
+	willMsg?: string;	// only used when will is true
+	willtopic?: string;	// only used when will is true
+	willQos?: QoS;	// only used when will is true
+	willRetainFlag: boolean;	// only used when will is true
+	automaticReconnect?: boolean;	// android only
+}
+
 export class IMqttClient {
-	constructor(options: {
-		clientId: string;
-		uri: string;
-		host?: string;
-		port?: number;
-		protocol?: 'mqtt' | 'tcp' | 'wss' | 'mqtts' | 'ws';
-		tls?: boolean;
-		keepalive?: number;	// seconds
-		protocolLevel?: number;
-		clean?: boolean;
-		auth?: boolean;
-		user?: string;	// only used when auth is true
-		pass?: string;	// only used when auth is true
-		will?: boolean;
-		willMsg?: string;	// only used when will is true
-		willtopic?: string;	// only used when will is true
-		willQos?: QoS;	// only used when will is true
-		willRetainFlag: boolean;	// only used when will is true
-		automaticReconnect?: boolean;	// android only
-	})
+	constructor(options: ClientOptions)
 	on(event: 'closed', cb: (msg: string) => void): void
 	on(event: 'error', cb: (msg: string) => void): void
 	on(event: 'message', cb: (msg: {
@@ -40,7 +42,7 @@ export class IMqttClient {
 }
 
 declare namespace mqtt {
-	function createClient(options: { uri: string, clientId?: string }): Promise<IMqttClient>;
+	function createClient(options: ClientOptions): Promise<IMqttClient>;
 	function removeClient(client: IMqttClient): void;
 	function disconnectAll(): void;
 }
